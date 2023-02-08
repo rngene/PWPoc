8bece1644b8eadb29c5d70e470542e6f450f347f
Ramon Gene <rgene@rccl.com>
2023-02-08 10:32:34 -0500

    Bind to results and display

--- src/components/countries.tsx
+++ src/components/countries.tsx
@@ -1,26 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { gql, useLazyQuery } from "@apollo/client";
+import Country from "@/models/country";
 
 const Countries = () => { 
 
     const [countryCode, setCountryCode] = useState<string>('US');
+    const [country, setCountry] = useState<Country|null>(null);
 
     const COUNTRIES_QUERY = gql`
     query Country($id: ID!) {
         country(code: $id) {
         name
-        native
         capital
-        emoji
         currency
-        languages {
-            code
-            name
-        }
-        }
+       }
     }`;
 
     const [getCountryDetails, { called, loading, data }] = useLazyQuery(COUNTRIES_QUERY);
+
+    useEffect(() => {
+        if (data) {
+            setCountry({
+                capital: data.country.capital,
+                currency: data.country.currency
+            });
+        }
+    },[data]);
 
     const handleChange = (selectedOption: React.ChangeEvent<HTMLSelectElement>)  => {
         setCountryCode(selectedOption.target.value);
@@ -34,6 +39,15 @@ const Countries = () => {
           </select>
       </span>
       <span><input type='button' value='submit' onClick={() => getCountryDetails({ variables: { id: countryCode } })}></input></span>
+      {country ? 
+         <>
+         <div>&nbsp;</div>
+            <div>Capital: <span>{country.capital}</span></div>
+            <div>Currency: <span>{country.currency}</span></div>
+         </>
+        : 
+         <></> }
+      
     </div>; 
 }
 
---
+++ src/models/country.ts
@@ -0,0 +1,6 @@
+interface Country {
+    capital: string,
+    currency: string
+}
+
+export default Country;
